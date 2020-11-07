27040af5d3944b952aad23da986d137f6541824e
Грязева Анастасия Анатольевна <AAnGryazeva@sberbank.ru>
2020-11-07 20:41:19 +0300

    ok

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ProfileInfo from './ProfileInfo';
 import ProfileSkills from './ProfileSkills';
+import  styles from './main.module.scss';
 
 export default function Main() {
   return (
-    <div>
+    <div className={styles.right}>
       <ProfileInfo />
       <hr/>
       <ProfileSkills />
