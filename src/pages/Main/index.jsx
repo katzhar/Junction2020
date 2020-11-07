93ea200a7c9c561321ab668afd6502440fa13b62
sergejij <ushakoff.sergej2010@yandex.ru>
2020-11-07 20:33:54 +0300

    profile and side bar

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ProfileInfo from './ProfileInfo';
 import ProfileSkills from './ProfileSkills';
+import classes from './Main.module.scss';
 
 export default function Main() {
   return (
-    <div>
+    <div className={classes.fullWidth}>
       <ProfileInfo />
-      <hr/>
+      <hr className={classes.line}/>
       <ProfileSkills />
     </div>
   );
