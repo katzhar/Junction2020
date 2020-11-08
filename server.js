20407e9d55b05062a0e936d28faabbec5b2f441a
a18574089 <EAZharovova@sberbank.ru>
2020-11-08 07:54:32 +0300

    ok

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const path = require('path');
-const favicon = require('express-favicon');
+// const favicon = require('express-favicon');
 const port = process.env.PORT || 8080;
 
 const app = express();
-app.use(favicon(__dirname + '/build/favicon.ico'));
+// app.use(favicon(__dirname + '/build/favicon.ico'));
 
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
