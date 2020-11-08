580261918758fcc10b68a7522718f75924b266ff
a18574089 <EAZharovova@sberbank.ru>
2020-11-08 07:45:25 +0300

    ok

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
-const dotenv = require('dotenv');
 const express = require('express');
- const port = process.env.PORT || 8080;
- 
- const app = express();
- 
- app.use(express.static(__dirname));
- app.use(express.static(path.join(__dirname, 'build')));
- 
- app.get('/*', function (req, res) {
- res.sendFile(path.join(__dirname, 'build', 'index.html'));
- });
- app.listen(port);
+const dotenv = require('dotenv');
+const path = require('path');
+const favicon = require('express-favicon');
+const port = process.env.PORT || 8080;
+
+const app = express();
+app.use(favicon(__dirname + '/build/favicon.ico'));
+
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port);
